7fd2bba2886d8c552c97c1887f31e0180fd05c7f
Sandeep Suresh <sandeepsuresh98@gmail.com>
2024-02-04 14:26:00 -0800

    Trying without RAG.py

--- src/queries/routes.py
+++ src/queries/routes.py
@@ -8,8 +8,10 @@ from llama_index import VectorStoreIndex
 from sanic import json
 from sanic_ext import cors
 from utils.rag import DecryptionNodePostProcessor
+from pinecone.core.client.configuration import Configuration as OpenApiConfiguration
 import os
 from utils.constants import PINECONE_ENV
+import pinecone 
 
 bp = Blueprint("queries")
 
@@ -18,10 +20,17 @@ bp = Blueprint("queries")
 @validate(query=QueryGenerate)
 async def create(request, query: QueryGenerate):
    print("Received query prompt: ", query.prompt)
-   app = Sanic.get_app(APP_NAME)
-   rag_ops = app.config.RAG_OPS
-   pinecone_index = rag_ops.get_index("journal")
+
+   pinecone_openapi_config = OpenApiConfiguration.get_default_copy()
+   pinecone.init(
+      api_key=os.getenv("PINECONE_API_KEY"),
+      environment=PINECONE_ENV,
+      openapi_config=pinecone_openapi_config
+   )
+   print(os.getenv("PINECONE_API_KEY"))
+   pinecone_index = pinecone.Index("journal")
    print("Created pinecone index", pinecone_index)
+
    vector_store = PineconeVectorStore(
       pinecone_index=pinecone_index,
       api_key=os.getenv("PINECONE_API_KEY"),
