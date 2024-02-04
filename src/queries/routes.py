0bdd4e318e22af455ff203ff33d4d108cdcae59e
Sandeep Suresh <sandeepsuresh98@gmail.com>
2024-02-04 13:10:12 -0800

    Testing Query Response

--- src/queries/routes.py
+++ src/queries/routes.py
@@ -15,30 +15,36 @@ bp = Blueprint("queries")
 @cors(origin="*")
 @validate(query=QueryGenerate)
 async def create(request, query: QueryGenerate):
-   print("Received query prompt: ", query.prompt)
-   app = Sanic.get_app(APP_NAME)
-   rag_ops = app.config.RAG_OPS
-   pinecone_index = rag_ops.get_index("journal")
-   vector_store = PineconeVectorStore(
-      pinecone_index=pinecone_index,
-   )
-   index = VectorStoreIndex.from_vector_store(vector_store)
-   # retriever = index.as_retriever()
-   # nodes = retriever.retrieve(query.prompt)
-   # for node in nodes:
-   #    print(node.text)
-   #    text = rag_ops.decrypt_text(node.text)
+   # print("Received query prompt: ", query.prompt)
+   # app = Sanic.get_app(APP_NAME)
+   # rag_ops = app.config.RAG_OPS
+   # pinecone_index = rag_ops.get_index("journal")
+   # vector_store = PineconeVectorStore(
+   #    pinecone_index=pinecone_index,
+   # )
+   # index = VectorStoreIndex.from_vector_store(vector_store)
+   # # retriever = index.as_retriever()
+   # # nodes = retriever.retrieve(query.prompt)
+   # # for node in nodes:
+   # #    print(node.text)
+   # #    text = rag_ops.decrypt_text(node.text)
 
-   query_engine = index.as_query_engine(
-      node_postprocessors=[
-         DecryptionNodePostProcessor(),
-      ]
-   )
-   query_response = query_engine.query(query.prompt)
+   # query_engine = index.as_query_engine(
+   #    node_postprocessors=[
+   #       DecryptionNodePostProcessor(),
+   #    ]
+   # )
+   # query_response = query_engine.query(query.prompt)
+   # return json(
+   #    {
+   #       "response": query_response.response,
+   #       "success": "true",
+   #    }
+   # )
+
    return json(
       {
-         "response": query_response.response,
-         "success": "true",
+         "response": "This is a test response",
       }
    )
    
